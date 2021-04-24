dea26b04197f99b47a29a93b7670d62c34cc9112
Anthony <zamora.anthony1234@gmail.com>
2021-04-23 23:39:28 -0400

    Added main file

---
+++ main.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+
+from bs4 import BeautifulSoup
+import requests
+
+url = "https://www.tutorialspoint.com/index.htm"
+req = requests.get(url)
+soup = BeautifulSoup(req.text, "html.parser")
+print(soup.title)
