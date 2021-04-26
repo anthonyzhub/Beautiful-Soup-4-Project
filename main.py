4510a27c5ff7e86da1be4df102a90dd42d50c91e
Anthony <zamora.anthony1234@gmail.com>
2021-04-26 18:21:57 -0400

    Can extract webpage's title and URL links

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 #! /usr/bin/python3
 
+# Source: https://www.tutorialspoint.com/beautiful_soup/beautiful_soup_souping_the_page.htm
+
 from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.tutorialspoint.com/index.htm"
 req = requests.get(url)
 soup = BeautifulSoup(req.text, "html.parser")
+
+# Extract webpage's title
 print(soup.title)
+
+# Print all URL links on the webpage
+for i in soup.find_all("a"):
+    print(i.get("href"))
